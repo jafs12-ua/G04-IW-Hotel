1a80f4b8132091585d200883f7adcf64c09503cf
Hector Hernandez <hhs1@alu.ua.es>
2025-12-05 18:53:41 +0100

    Adición de las páginas de login y register

--- src/main/java/com/villadictos/app/controller/HomeController.java
+++ src/main/java/com/villadictos/app/controller/HomeController.java
@@ -11,4 +11,14 @@ public class HomeController {
     public String index(Model model) {
         return "index";
     }
+    
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "login";
+    }
+    
+    @GetMapping("/register")
+    public String register(Model model) {
+        return "register";
+    }
 }
